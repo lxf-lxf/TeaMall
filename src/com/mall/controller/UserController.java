cb1f0bc5268be8da41b18ec6fceb695ddfcb9371
lxf <61101384+lxf-lxf@users.noreply.github.com>
2020-08-18 14:45:29 +0800

    Update UserController.java
    ymf_个人信息修改&忘记密码处理&积分列表1.0

--- src/com/mall/controller/UserController.java
+++ src/com/mall/controller/UserController.java
@@ -29,9 +29,11 @@ public class UserController {
     //用户登录
     @RequestMapping("/login")
     public String login(String uname, String password, HttpSession session, Model model){
+        System.out.println(uname+"---"+password);
         User user = userService.login(uname, password);
         List<Goods> goods = goodsService.selAllGoods();
-        if(user != null && goods != null){
+        System.out.println(goods.get(0).toString());
+        if(user != null && goods!=null){
             model.addAttribute("goods",goods);
             session.setAttribute("user",user);
             return "/index";
@@ -69,6 +71,7 @@ public class UserController {
     @RequestMapping("/phoneRegister")
     @ResponseBody
     public String selPhone(String phone) throws IOException {
+        System.out.println(phone);
         user = userService.selPhone(phone);
         //发送验证码的类
         PhoneNumFactory num = new PhoneNumFactory();
@@ -93,20 +96,71 @@ public class UserController {
         int n = userService.register(user);
         if(n == 1) {
             model.addAttribute("user",u);
-            return "/index";
+            return "redirect:/index";
         }else{
             return "/register";
         }
     }
 
-    @RequestMapping(value = "/updatePwd",produces = "text/html;charset=utf-8")
+    //ymf-8.15
+    //查询个人信息
+    @RequestMapping(value = "/ShowUserInfo",produces = "text/html;charset=utf-8")
+    public String ShowUserInfo(HttpSession session,Model model){
+        User user = (User) session.getAttribute("user");
+        int uid = user.getUid();
+        User user1 = userService.ShowUserInfo(uid);
+        model.addAttribute("user",user1);
+        return "/user_info";
+    }
+
+    //修改个人信息
+    @RequestMapping(value = "/UpdateUser",produces = "text/html;charset=utf-8")
     @ResponseBody
-    public String updatePwd(String uid,String newPwd){
-        int n = userService.updatePwd(Integer.parseInt(uid), newPwd);
-        if(n == 1) {
-            return "修改成功";
-        }else {
-            return "修改失败";
+    public String UpdateUser(int uid,String uname,String realname,String sex,String phone,String email){
+        User user = new User();
+        user.setUid(uid);
+        user.setUname(uname);
+        user.setRealname(realname);
+        user.setSex(sex.charAt(0));
+        user.setPhone(phone);
+        user.setEmail(email);
+        int i = userService.UpdateUser(user);
+        return Integer.toString(i);
+    }
+    //
+    //校验手机号是否注册不发送验证码
+    @RequestMapping("/phoneIsExist")
+    @ResponseBody
+    public String phoneIsExist(String phone) throws IOException {
+        user = userService.selPhone(phone);
+        if(user!=null){
+            return "1";
+        }else{
+            return "0";
+        }
+    }
+    //忘记密码发送验证码
+    @RequestMapping(value = "/sendCode",produces = "text/html;charset=utf-8")
+    @ResponseBody
+    public String sendCode(String phone) throws IOException {
+        PhoneNumFactory phoneNumFactory = new PhoneNumFactory();
+        code = phoneNumFactory.numFactory();
+        System.out.println("code:"+code);
+        return code;
+    }
+    //忘记密码设置密码
+    @RequestMapping(value = "/resetPwd",produces = "text/html;charset=utf-8")
+    @ResponseBody
+    public String resetPwd(String phone){
+        PhoneNumFactory phoneNumFactory = new PhoneNumFactory();
+        String password=phoneNumFactory.generatePassword(6);//生出随机6位数密码
+        System.out.println("pwd:"+password);
+        int i = userService.reserPwd(phone, password);
+        System.out.println(i);
+        if (i==1){
+            return password;
+        }else{
+            return "0";
         }
     }
 }
